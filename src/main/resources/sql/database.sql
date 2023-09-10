9002e2d2366fecb3e5fb8f2d275b232891e2a6f4
LittleJhey <JustALittleJhey@gmail.com>
2023-09-09 23:01:51 -0300

    Updated sql

--- src/main/resources/sql/database.sql
+++ src/main/resources/sql/database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE bruno_feio;
 CREATE DATABASE bruno_feio;
 USE bruno_feio;
 
@@ -10,4 +11,27 @@ streetName varchar(200) not null,
 addressNumber varchar(20) not null,
 additionalInfo varchar(200) not null,
 gender varchar(20) not null
+);
+
+create table seller(
+id varchar(36) primary key not null,
+name varchar(200) not null,
+zipCode varchar(20) not null,
+streetName varchar(200) not null,
+addressNumber varchar(20) not null
+);
+
+create table product(
+id varchar(36) primary key not null,
+name varchar(200) not null,
+
+description text default "",
+
+seller_fk varchar(36) not null,
+price numeric(9, 2) not null,
+country varchar(100) not null,
+quantity numeric(9,3) not null,
+quantified_as char(3),
+
+foreign key (seller_fk) references seller(id)
 );
